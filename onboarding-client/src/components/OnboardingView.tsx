67296713b062edb14206e5c245b176041d3d2bc3
Fabrice LAMARE <fabrice.lamare@finaptic.com>
2022-05-09 13:23:48 +0400

    bundle label in OnboardingView

--- onboarding-client/src/components/OnboardingView.tsx
+++ onboarding-client/src/components/OnboardingView.tsx
@@ -2,11 +2,39 @@ import React from "react";
 import Select from 'react-select'
 import OnboardingForm from "./Views/OnboardingForm";
 import OnboardingProcess from "./Views/OnboardingProcess";
+import { Bundles } from '../models/Onboarding/domain/Bundles'
 
 export default class OnboardingView extends React.Component <any, any> {
+
+    bundleLabels = new Map<string, string>([
+        [Bundles.productlessV1, "product less"],
+        [Bundles.prepaidV1, "prepaid"],
+        [Bundles.depositV1, "deposit"],
+        [Bundles.kycV1, "kyc"],
+        [Bundles.creditCardV1, "credit card"],
+        [Bundles.ulocMvp1, "uloc"],
+    ]);
+
+    allBundles = () => {
+        let options: {value: string, label: string} []
+        options = []
+
+        let bundles: string[]
+        console.log(this.props.allBundles)
+        bundles = this.props.allBundles
+        bundles.forEach(bundleKey => {
+           let label = this.bundleLabels.get(bundleKey)
+           if (label !== undefined) {
+               var option = {value: bundleKey, label: label}
+               options.push(option)
+           }
+        })
+
+       return options
+    }
+
     render() {
         const {
-            allBundles,
             onBundleSelected,
             bundleSelected,
             createProduct
@@ -18,7 +46,7 @@ export default class OnboardingView extends React.Component <any, any> {
 
                 <div className="onboarding-infos-container">
                 Product bundle:
-                    <Select className="select" onChange={onBundleSelected} options={allBundles} />
+                    <Select className="select" onChange={onBundleSelected} options={this.allBundles()} />
 
                     <OnboardingForm bundle={bundleSelected} />
 
